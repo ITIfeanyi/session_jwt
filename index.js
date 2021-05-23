fd1459f781a16add9500652a6534c8df0a7cbbe0
ITIfeanyi <ifeanyithankgodigweh@gmail.com>
2021-05-23 13:55:45 -0700

    added process.evn.PORT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cookieParser = require("cookie-parser");
 const path = require("path");
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 require("./model/db");
 
 app.use(cookieParser());
